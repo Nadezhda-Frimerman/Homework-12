2bd603f42ca3b6a4bbbe7a92b0f6700ba6c828dd
Nadezhda-Frimerman <tomns@mail.ru>
2024-05-06 20:35:54 +0500

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) {
+        Author author1 = new Author("Иван", "Ефремов");
+        Author author2 = new Author("Леон", "Фейхтвангер");
+        Book book1 = new Book("Лезвие бритвы", author1, 1976);
+        Book book2 = new Book("Испанская Баллада", author2, 1969);
+        book1.setYearOfPublishing(1982);
+
+    }
+}
+
+
